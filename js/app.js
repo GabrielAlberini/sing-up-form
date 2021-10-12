807fd7298da287e33278dad1fda68d615eb62d22
CuerpoCasa <gabialberini733@gmail.com>
2021-10-12 17:50:58 -0300

    V1.2 Finished project, only the Readme is missing

--- js/app.js
+++ js/app.js
@@ -1,51 +1,78 @@
+//Recognizing the elements that I will to work.
 const iFName = document.querySelector('#iFName');
 const iLName = document.querySelector('#iLName');
 const iEmail = document.querySelector('#iEmail');
 const iPass = document.querySelector('#iPass');
 const submit = document.querySelector('#submit');
 
+//Messages that I will show in case that the entry it's not accepted.
+const messages = ['Ok!', //Message if everything is Ok.
+'First Name cannot be empty!', //Message for First Name in case that it be empty.
+'Last Name cannot be empty!', //Message for Lirst Name in case that it be empty.
+'E-mail cannot be empty!', //Message for Email in case that it be empty.
+'Looks like this is not an email!', //Message for Emailin case that it be not be a valid email.
+'Password cannot be empty!'] //Message for Pass in case that it be empty.
+
+//Completed process at the time to make "clicking" in the button to submit.
 submit.addEventListener('click', e => {
-    e.preventDefault()
-    checkImputs()
+    e.preventDefault();
+    checkImputs();
+    cleanForm();
 })
 
+//Checking the inputs
 function checkImputs() {
-    const checkiFName = iFName.value.trim();
-    const checkiLName = iLName.value.trim();
-    const checkiEmail = iEmail.value.trim();
-    const checkiPass = iPass.value.trim();
-
-    if(checkiFName === '') {
-        setError(iFName, 'First Name cannot be empty!')
+    if(iFName.value.trim() === '' || iFName.value.trim() === null) {
+        setError(iFName, messages[1]);
     } else {
-        setSuccess(iFName, 'Ok!')
+        setSuccess(iFName, messages[0]);
     }
 
-    if(checkiLName === '') {
-        setError(iLName, 'Last Name cannot be empty!')
+    if(iLName.value.trim() === '' || iLName.value.trim() === null) {
+        setError(iLName, messages[2]);
     } else {
-        setSuccess(iLName, 'Ok!')
+        setSuccess(iLName, messages[0]);
     }
 
-    if(checkiEmail === '') {
-        setError(iEmail, 'E-mail cannot be empty!')
-    } else if(!validateEmail(emailValue)) {
-        setError(iEmail, 'Looks like this is not an email!')
+    if(iEmail.value.trim() === '' || iEmail.value.trim() === null) {
+        setError(iEmail, messages[3]);
+    } else if(!validateEmail(iEmail.value.trim())) {
+        setError(iEmail, messages[4]);
     } else {
-        setSuccess(email, 'Ok!')
+        setSuccess(iEmail, messages[0]);
     }
 
-    if(checkiPass === '') {
-        setError(iPass, 'Password cannot be empty!')
+    if(iPass.value.trim() === '' || iPass.value.trim() === null) {
+        setError(iPass, messages[5]);
     } else {
-        setSuccess(iPass, 'Ok!')
+        setSuccess(iPass, messages[0]);
     }
 }
 
+//If there a error, to show this message.
 function setError(input, message) {
     const formControl = input.parentElement;
-    const messageError = formControl.querySelector('span');
+    const small = formControl.querySelector('small');
 
-    formControl.className = 'cont-input error';
-    messageError.innerText = message;
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+//If there a success, to show this message.
+function setSuccess(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    formControl.className = 'form-control success';
+    small.innerText = message;
+}
+//Validation with regex for emails.
+function validateEmail(iEmail) {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    return re.test(String(iEmail).toLowerCase());
+}
+//After completing the form, this is cleaned. Yet I can´t take out his styles. Help please haha :)
+function cleanForm() {
+    const form = document.getElementById("cont-form");
+    form.reset();
 }
